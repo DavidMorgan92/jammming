267b739765aafb22a2a758309e6200700356e3de
David Morgan <david.morgan@europe.com>
2021-12-13 15:28:03 +0000

    Add key to track list

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,6 +6,7 @@ export class TrackList extends React.Component {
 	render() {
 		const tracks = this.props.tracks.map(
 			track => <Track
+				key={track.id}
 				track={track}
 				onAdd={this.props.onAdd}
 				onRemove={this.props.onRemove}
